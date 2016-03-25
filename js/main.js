d35d4e50de74e6c1f5b8c59f93a98e75cc3a5d13
Erik Kennedy <erik.d.kennedy@gmail.com>
2016-03-25 10:00:29 -0700

    final submission, tested on iOS devices

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
 
-	/*****************************************
-					LISTENERS
-	*****************************************/
-
 	//PROTOTYPE ONLY: make sure href="#" links don't go to top of page
 	$("a[href=#]").click(function(e) {
 		e.preventDefault();
 	});
 
-	//PROTOTYPE ONLY: switch between Philo found and not found
+	//PROTOTYPE ONLY: switch between Philo found/not found and cookie found/not found
 	$(document).keyup(function(e) {
 		//switch if "p" is pressed
 		if (e.keyCode === 80) {
@@ -19,16 +15,6 @@ $(document).ready(function() {
 		//switch if "c" is pressed
 		if (e.keyCode === 67) {
 			$("body").toggleClass("cookie-yes");
-		}
-	});
-
-	//on scroll
-	$(document).scroll(function() {
-		//add dark translucent bg to header if scrolled
-		if ($(this).scrollTop() > 0) {
-			$("header").addClass("is-scrolled");
-		} else {
-			$("header").removeClass("is-scrolled");
 		}
 	});
 
@@ -43,13 +29,60 @@ $(document).ready(function() {
 	});
 
 	//click dropdown link
-	$(".dropdown > a").click(function(e) {
-		$(this).addClass("active");
-		e.stopPropagation();
+	$(".dropdown > a").click(function(clickEvent) {
+		if ($(this).hasClass("active")) {
+			hideAllDropdowns();
+		} else {
+			hideAllDropdowns();
+			$(this).addClass("active");
+			clickEvent.stopPropagation();
+		}
 	});
 
 	//click anywhere to close dropdown
-	$(document).click(function() {
-		$(".dropdown > a.active").removeClass("active");
+	$(document).on("click touchstart", function() {
+		hideAllDropdowns();
 	});
+
+	function hideAllDropdowns() {
+		$("li.dropdown > a.active").removeClass("active");
+	}
+
+    //on scroll, update parallaxed positions
+    $(window).on("scroll resize", function() {
+
+    	if (Modernizr.touch) return;
+
+        var scrollDist = $(window).scrollTop();
+
+        $("[data-parallax]").each(function() {
+            var offset = $(this).attr("data-offset");
+            var speed = $(this).attr("data-speed");
+            var translation =  -(offset-scrollDist)*speed;
+
+            if ($(this).attr("data-axis") === "x") {
+                $(this).css("transform", "translateX(" + translation + "px)");
+            } else {
+                $(this).css("transform", "translateY(" + translation + "px)");
+            }
+        });
+    });
+
+	//initialize parallax elements
+    $("[data-parallax]").each(function() {
+
+    	if (Modernizr.touch) return;
+
+        var offset = $(this).offset().top - 0.5*$(window).height();
+        var speed = $(this).attr("data-speed");
+        var translation = -offset*speed;
+
+        $(this).attr("data-offset", offset);
+
+        if ($(this).attr("data-axis") === "x") {
+            $(this).css("transform", "translateX(" + translation + "px)");
+        } else {
+            $(this).css("transform", "translateY(" + translation + "px)");
+        }
+    });
 });
